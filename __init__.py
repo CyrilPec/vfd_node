df26309a97f0e87dfc3c0a13ebf4f457de3574e6
CyrilPec <95511181+CyrilPec@users.noreply.github.com>
2026-09-06 09:17:03 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,20 +1,23 @@
 bl_info = {
     "name": "VFD Node",
     "author": "CyrilPec",
-    "version": (1, 1, 0),
+    "version": (1, 2, 0),
     "blender": (3, 0, 0),
     "location": "Node Editor > Add > VFD",
     "description": "Control a Huanyang HY01D523B VFD from Blender.",
     "category": "Node",
 }
+
 from . import vfd_node
+
 
 def register():
     vfd_node.register()
 
+
 def unregister():
     vfd_node.unregister()
 
+
 if __name__ == "__main__":
     register()
-
