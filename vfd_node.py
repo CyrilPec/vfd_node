1b1641c5cadc474c597b73baae52671101415c50
CyrilPec <95511181+CyrilPec@users.noreply.github.com>
2026-09-10 09:29:01 +0300

    Refactor VFD API retrieval functions
    Removed the get_vfd_api function and modified acquire_vfd_api to use instance variables.

--- vfd_node.py
+++ vfd_node.py
@@ -52,46 +52,9 @@ class _VFDEntry:
         self.api = api
         self.users = 0
 
-
-def get_vfd_api(
-    port: str,
-    slave_id: int,
-) -> VFDAPI:
-
-    key = _api_key(
-        port,
-        slave_id,
-    )
-
-    entry = _VFD_REGISTRY.get(key)
-
-    if entry is None:
-
-        session = VFDSession(
-            port=key[0],
-            slave_id=key[1],
-        )
-
-        manager = VFDManager(
-            session=session,
-        )
-
-        api = VFDAPI(
-            manager=manager,
-        )
-
-        entry = _VFDEntry(
-            api=api,
-        )
-
-        _VFD_REGISTRY[key] = entry
-
-    return entry.api
-
-
 def acquire_vfd_api(
-    port: str,
-    slave_id: int,
+    port=self.port,
+    slave_id=self.slave_id,
 ) -> VFDAPI:
 
     key = _api_key(
